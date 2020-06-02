8802afba524b859ea02953535ec908da94bd617d
Chandrakant Yadav <chndyadav@gmail.com>
2020-06-02 20:09:53 +0530

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -134,5 +134,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'chndyadav@gmail.com'
-EMAIL_HOST_PASSWORD = 'ywxifeubnzpdphlh'
+EMAIL_HOST_USER = 'username@gmail.com'
+EMAIL_HOST_PASSWORD = 'user_password'
